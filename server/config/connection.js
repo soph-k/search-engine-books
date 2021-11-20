f0b9a8a1d997cd20a0d000b453dfaf95ce133f44
Sophia <sophkiani@gmail.com>
2021-11-20 11:07:28 -0600

    fixed bugs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  process.env.MONGODB_URI || 'mongodb://localhost/searchbooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
